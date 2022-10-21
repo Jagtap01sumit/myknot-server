460294c7c4c3c3a8ed3c843ddcbdd956b0bbf4df
Leston <lestonair6574@gmail.com>
2022-10-21 18:10:46 +0530

    sixth one

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const cors=require("cors")
 connectDB()
 const app=express()
 
-// app.use(cors({
-//     origin:"https://myknot-official.netlify.app"
-// }))
 app.use(cors({
-    origin:"http://localhost:3000"
+    origin:"https://myknot-official.netlify.app"
 }))
+// app.use(cors({
+//     origin:"http://localhost:3000"
+// }))
 
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({limit:"50mb",extended:true}))
