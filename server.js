7dcf1e093e5091fcaa48357cc1b00930ee968802
Leston <lestonair6574@gmail.com>
2022-11-28 22:45:45 +0530

    cloudinary changes

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const cors=require("cors")
 connectDB()
 const app=express()
 
-app.use(cors({
-    origin:"https://myknot-official.netlify.app",
-    // credentials: true
-}))
 // app.use(cors({
-//     origin:"http://localhost:3000"
+//     origin:"https://myknot-official.netlify.app",
+//     // credentials: true
 // }))
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
 
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({limit:"50mb",extended:true}))
@@ -28,6 +28,7 @@ app.use("/api/auth",require("./routes/userRoutes.js"))
 app.use("/api/img",require("./routes/imageRoutes.js"))
 app.use("/api/themes",require("./routes/themeRoutes.js"))
 app.use("/api/cart",require("./routes/cartRoutes.js"))
+app.use("/api/payement",require("./routes/payementRoutes.js"))
 
 app.use(ErrorResponse)
 
