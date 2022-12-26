51127603080d08010566d5c394281e09d5a25bc2
Leston <lestonair6574@gmail.com>
2022-12-26 22:51:50 +0530

    add versel json

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const cors=require("cors")
 connectDB()
 const app=express()
 
-app.use(cors({
-    origin:"https://myknot-official.netlify.app",
-    // credentials: true
-}))
 // app.use(cors({
-//     origin:"http://localhost:3000"
+//     origin:"https://myknot-official.netlify.app",
+//     // credentials: true
 // }))
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
 
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({limit:"50mb",extended:true}))
@@ -30,6 +30,7 @@ app.use("/api/themes",require("./routes/themeRoutes.js"))
 app.use("/api/cart",require("./routes/cartRoutes.js"))
 app.use("/api/payement",require("./routes/payementRoutes.js"))
 app.use("/api/orders",require("./routes/orderRoutes.js"))
+app.use("/api/category",require("./routes/categoryRoutes.js"))
 
 app.use(ErrorResponse)
 
